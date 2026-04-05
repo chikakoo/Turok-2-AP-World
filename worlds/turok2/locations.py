7f004b96c725cc9e6fc93fb685dc1172d9b4ff61
Chikakoo <lionel.crafts@gmail.com>
2026-04-04 22:54:58 -0500

    Use events for Primagen Key 6

--- worlds/turok2/locations.py
+++ worlds/turok2/locations.py
@@ -318,20 +318,11 @@ def guarantee_torpedo_launcher(world: Turok2World):
     """Checks whether the Torpedo Launcher is in logic"""
     guarantee_torpedo_launcher = world.options.guarantee_torpedo_launcher
     return lambda state: guarantee_torpedo_launcher
-
-def can_reach_level_6_primagen_key(world: Turok2World):
-    """Checks whether the level 6 primagen key switches can be pressed"""
-    return lambda state: (
-        state.can_reach_region("PL Wing 1 Whispers", world.player) and
-        state.can_reach_region("PL Wing 2 Eye of Truth", world.player) and
-        state.can_reach_region("PL Wing 3 Leap of Faith", world.player) and
-        state.can_reach_region("PL Wing 4 Generator River", world.player))
     
 NAMED_RULES = {
     "weapon_requirement": weapon_requirement,
     "vanilla_mission_items": vanilla_mission_items,
     "mission_item_requirement": mission_item_requirement,
     "open_hub": open_hub,
-    "guarantee_torpedo_launcher": guarantee_torpedo_launcher,
-    "can_reach_level_6_primagen_key": can_reach_level_6_primagen_key
+    "guarantee_torpedo_launcher": guarantee_torpedo_launcher
 }
